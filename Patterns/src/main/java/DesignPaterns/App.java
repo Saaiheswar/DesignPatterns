c162ce1d2e14a6d262f83e24693d5b803c0d3aa4
Saaiheswar <saaiheswar@gmail.com>
2020-03-12 00:02:54 +0530

    1st commit

---
+++ Patterns/src/main/java/DesignPaterns/App.java
@@ -0,0 +1,13 @@
+package DesignPaterns;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
